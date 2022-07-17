d4d9f0adb6268f0e5af87bae8e37585058ba92f3
MuhammadLuqman--99 <you@example.com>
2022-07-17 13:46:02 +0800

    update nft fcc

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -5,33 +5,110 @@ pragma solidity ^0.8.8;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "base64-sol/base64.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
 
-contract DynamicSvgNft is ERC721 {
+contract DynamicSvgNft is ERC721, Ownable {
     // Mint
     // Store our SVG information somewhere
     // Some logic to say "Show X Image" or "Show Y Image"
 
     uint256 private s_tokenCounter;
-    string private i_lowImageUri;
-    string private i_highImageUri;
+    string private s_lowImageUri;
+    string private s_highImageUri;
     string private constant base64EncodedSvgPrefix = "data:image/svg+xml;base64,";
 
-    constructor(string memory lowSvg, string memory highSvg) ERC721("Dynamic SVG NFT", "DSN") {
+    AggregatorV3Interface internal immutable i_priceFeed;
+    mapping(uint256 => int256) public s_tokenIdToHighValues;
+
+    event CreatedNFT(uint256 indexed tokenId, int256 highValue);
+
+    constructor(
+        address priceFeedAddress,
+        string memory lowSvg,
+        string memory highSvg
+    ) ERC721("Dynamic SVG NFT", "DSN") {
         s_tokenCounter = 0;
+        s_lowImageUri = svgToImageURI(lowSvg);
+        s_highImageUri = svgToImageURI(highSvg);
+        i_priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
     // You could also just upload the raw SVG and have solildity convert it!
     function svgToImageURI(string memory svg) public pure returns (string memory) {
         // example:
         // '<svg width="500" height="500" viewBox="0 0 285 350" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="black" d="M150,0,L75,200,L225,200,Z"></path></svg>'
-        // would return 
+        // would return
         string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
         return string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
-
     }
 
-    function mintNft() public {
+    function mintNft(int256 highValue) public {
+        s_tokenIdToHighValues[s_tokenCounter] = highValue;
+        emit CreatedNFT(s_tokenCounter, highValue);
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenCounter = s_tokenCounter + 1;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        /* string memory imageURI = "Hi"; */
+
+        // data:image/svg+xml;base64,
+        // data:application/json;base64,
+
+        (, int256 price, , , ) = i_priceFeed.latestRoundData();
+        string memory imageURI = s_lowImageUri;
+        if (price >= s_tokenIdToHighValues[tokenId]) {
+            imageURI = s_highImageUri;
+        }
+        // lps siap semua bwah return dpt lah kita token uri seperti
+        // Cnth :
+        // data:application/json;base64;base64,cbaiosIBohihHigfCYr6ihGYDhufyU&UFKhifguFugi
+
+        return
+            // tukar semua dkt bawah tu jadi string
+            string(
+                abi.encodePacked(
+                    // pin di _baseURI
+                    _baseURI(),
+                    // incode in base 64
+                    Base64.encode(
+                        // encode in bytes
+                        bytes(
+                            abi.encodePacked(
+                                // create json string
+                                '{"name":"',
+                                name(), // You can add whatever name here
+                                '", "description":"An NFT that changes based on the Chainlink Feed", ',
+                                '"attributes": [{"trait_type": "coolness", "value": 100}], "image":"',
+                                imageURI,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
+    }
+
+    function getLowSVG() public view returns (string memory) {
+        return s_lowImageUri;
+    }
+
+    function getHighSVG() public view returns (string memory) {
+        return s_highImageUri;
+    }
+
+    function getPriceFeed() public view returns (AggregatorV3Interface) {
+        return i_priceFeed;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
     }
 }
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,7 +1,7 @@
 const networkConfig = {
     31337: {
         name: "localhost",
-        ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
+        ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
         mintFee: "10000000000000000", // 0.01 ETH
         callbackGasLimit: "500000", // 500,000 gas
